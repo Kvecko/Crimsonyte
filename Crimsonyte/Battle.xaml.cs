72e87ea371847dc9d0a4672e8b68aa4c30ca035c
Tomáš Pabián <tompa.pabi@gmail.cz>
2024-05-09 13:11:27 +0200

    Combat update Complated Combat (?) + its visuals

--- Crimsonyte/Battle.xaml.cs
+++ Crimsonyte/Battle.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,130 @@ namespace Crimsonyte
         {
             InitializeComponent();
             labelPlayerName.Content = Stats.playerName;
+            barPlayerHP.Maximum = Stats.playerHP;
+            barPlayerHP.Value = Stats.playerHP;
+
+            labelEnemyName.Content = "Lynette";
+            Stats.enemyHP = 100;
+            Stats.enemyDMGMin = 5;
+            Stats.enemyDMGMax = 10;
+            barEnemyHP.Maximum = Stats.enemyHP;
+            barEnemyHP.Value = Stats.enemyHP;
+
+            labelWave.Content = Stats.wave + ".";
+        }
+
+        private void btnAttack_Click(object sender, RoutedEventArgs e)
+        {
+            if(Stats.usedAttack == false)
+            {
+                Stats.usedAttack = true;
+
+                Stats.value = Stats.rng.Next(25, 31);
+                MessageBox.Show($"You dealt {Stats.value} damage");
+                Stats.enemyHP -= Stats.value;
+                barEnemyHP.Value = Stats.enemyHP;
+            }
+            else
+            {
+                MessageBox.Show("Already used move");
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            Stats.usedAttack = false;
+            if (barEnemyHP.Value <= 0)
+            {
+                Stats.wave++;
+                labelWave.Content = Stats.wave + ".";
+                switch (Stats.wave)
+                {
+                    case 2:
+                        labelEnemyName.Content = "Lyney";
+                        enemyPic1.Visibility = Visibility.Hidden;
+                        enemyPic2.Visibility = Visibility.Visible;
+                        Stats.enemyHP = 125;
+                        Stats.enemyDMGMin = 15;
+                        Stats.enemyDMGMax = 20;
+                        break;
+                    case 3:
+                        labelEnemyName.Content = "Arlecchino";
+                        enemyPic2.Visibility = Visibility.Hidden;
+                        enemyPic3.Visibility = Visibility.Visible;
+                        Stats.enemyHP = 175;
+                        Stats.enemyDMGMin = 20;
+                        Stats.enemyDMGMax = 30;
+                        break;
+                    case 4:
+                        MessageBox.Show("You won!");
+                        //SEM DODAT OTEVŘENÍ WIN SCREENU
+                        MessageBox.Show("The project is now going to self-destruct!");
+                        Close();
+                        break;
+                }
+                barEnemyHP.Maximum = Stats.enemyHP;
+                barEnemyHP.Value = Stats.enemyHP;
+            }
+            else
+            {
+                Stats.value = Stats.rng.Next(Stats.enemyDMGMin, Stats.enemyDMGMax + 1);
+                Stats.playerHP -= Stats.value;
+                barPlayerHP.Value = Stats.playerHP;
+
+                if(Stats.playerHP <= 0)
+                {
+                    MessageBox.Show("YOU DIED");
+                    //Vyměnit MessageBox za Loss screen
+                    MessageBox.Show("The project is now going to self-destruct!");
+                    Close();
+                }
+            }
+        }
+
+        private void btnParry_Click(object sender, RoutedEventArgs e)
+        {
+            if (Stats.usedAttack == false)
+            {
+                Stats.usedAttack = true;
+
+                Stats.value = Stats.rng.Next(0, 101);
+                if(Stats.value <= 60)
+                {
+                    Stats.value = Stats.rng.Next(Stats.enemyDMGMin, Stats.enemyDMGMax + 1);
+                    MessageBox.Show($"You failed the parry and took {Stats.value} damage");
+                    Stats.playerHP -= Stats.value;
+                    barPlayerHP.Value = Stats.playerHP;
+                }
+                else
+                {
+                    Stats.value = Stats.rng.Next(40, 51);
+                    MessageBox.Show($"You succesfully paried and dealt {Stats.value} damage");
+                    Stats.enemyHP -= Stats.value;
+                    barEnemyHP.Value = Stats.enemyHP;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Already used move");
+            }
+        }
+
+        private void btnHeal_Click(object sender, RoutedEventArgs e)
+        {
+            if (Stats.usedAttack == false)
+            {
+                Stats.usedAttack = true;
+
+                Stats.value = Stats.rng.Next(30, 51);
+                MessageBox.Show($"You healed for {Stats.value} HP");
+                Stats.playerHP += Stats.value;
+                barPlayerHP.Value = Stats.playerHP;
+            }
+            else
+            {
+                MessageBox.Show("Already used move");
+            }
         }
     }
 }
--- Crimsonyte/Stats.cs
+++ Crimsonyte/Stats.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Crimsonyte
 {
     internal class Stats
     {
+        //Game values
+        public static Random rng = new Random();
+        public static int value;
+        public static int wave = 1;
+
+        //Player things
         public static string playerName { get; set; }
+        public static int playerHP = 150;
+        public static bool usedAttack = false;
+
+        //Enemy things
+        public static int enemyHP;
+        public static int enemyDMGMin;
+        public static int enemyDMGMax;
     }
 }
